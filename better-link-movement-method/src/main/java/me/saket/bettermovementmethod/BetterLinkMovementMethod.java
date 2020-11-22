c10dff357a94cdbc7be755da8ef17598bac84085
Jarmo Kukkola <last.do.app@gmail.com>
2020-11-22 22:28:22 +0100

    Fixed missing semicolon.

--- better-link-movement-method/src/main/java/me/saket/bettermovementmethod/BetterLinkMovementMethod.java
+++ better-link-movement-method/src/main/java/me/saket/bettermovementmethod/BetterLinkMovementMethod.java
@@ -386,7 +386,7 @@ public class BetterLinkMovementMethod extends LinkMovementMethod {
   protected void dispatchUrlClick(TextView textView, ClickableSpan clickableSpan) {
     if(clickableSpan != null){
       if(onLinkClickListener!=null){
-        onLinkClickListener.onClick(textView,null)
+        onLinkClickListener.onClick(textView,null);
       }
     }else{
       ClickableSpanWithText clickableSpanWithText=ClickableSpanWithText.ofSpan(textView,clickableSpan);
